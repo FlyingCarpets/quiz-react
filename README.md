3666324251ce608b06d88fddd22d6cf6161bb4cd
Rita <rita.bureikaite@nfq.lt>
2017-04-24 17:36:01 +0300

    react router add

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 * node_modules/.bin/webpack --config react.config.js
 * node_modules/.bin/webpack --config react.config.js --watch
+
+### Running webpack with webpack-dev-server
+* node_modules/.bin/webpack-dev-server --config react.config.js --watch
--- react.js
+++ react.js
@@ -1,6 +1,20 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 import TaskBuilder from './components/TaskBuilder/TaskBuilder';
 
-ReactDOM.render(<TaskBuilder/>, document.getElementById('app'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/music">Music</Link></li>
+                <li><Link to="/visual-art">Visual Art</Link></li>
+                <li><Link to="/literature">Literature</Link></li>
+            </ul>
+
+            <Route  path="/music" component={TaskBuilder}/>
+        </div>
+    </Router>,
+    document.getElementById('app')
+);
