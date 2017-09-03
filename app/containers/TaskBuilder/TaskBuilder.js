7d00a31aa242018b598fdf76fd8b98fbfb64c206
Rita B <rita.bureikaite@transfergo.com>
2017-09-03 12:28:00 +0300

    json url change

--- app/containers/TaskBuilder/TaskBuilder.js
+++ app/containers/TaskBuilder/TaskBuilder.js
@@ -22,7 +22,7 @@ class TaskBuilder extends React.Component {
     }
     fetchData() {
         numberList = [];
-        fetch('https://raw.githubusercontent.com/FlyingCarpets/quiz-react/master/data/questions.json')
+        fetch('https://raw.githubusercontent.com/FlyingCarpets/quiz-react/master/assets/data/questions.json')
             .then((response) => response.json())
             .then(json => {
                 this.setState({questions: json});
